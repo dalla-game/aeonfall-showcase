c14c149c3e3085fb9ff7e639a6db7c1ea8ef93f2
blacksmith01 <hahakickkick@hotmail.com>
2026-04-12 12:32:31 +0900

    steam link update

--- src/BlazorApp/Models/SiteProperties.cs
+++ src/BlazorApp/Models/SiteProperties.cs
@@ -7,6 +7,7 @@ public class SiteProperties
     public string Subtitle { get; set; } = string.Empty;
     public string Company { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
+    public string Rights { get; set; } = string.Empty;
     public string DevDotTo { get; set; } = string.Empty;
     public string GitHub { get; set; } = string.Empty;
     public string Instagram { get; set; } = string.Empty;
@@ -14,4 +15,7 @@ public class SiteProperties
     public string Medium { get; set; } = string.Empty;
     public string Twitter { get; set; } = string.Empty;
     public string YouTube { get; set; } = string.Empty;
+    public string Steam { get; set; } = string.Empty;
+    public string EpicMegaGrants1 { get; set; } = string.Empty;
+    public string EpicMegaGrants2 { get; set; } = string.Empty;
 }
